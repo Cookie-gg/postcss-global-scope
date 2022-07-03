0ea684cccb281f3e9e8beb518eb7fa2df8a7d419
Cookie_gg <cookie.gg.dev@gmail.com>
2022-07-04 01:54:09 +0900

    remove unecessary regexp

--- src/libs/regexp/index.ts
+++ src/libs/regexp/index.ts
@@ -1,7 +1,4 @@
 export const REGEXP = {
-  CUSTOM_PROPERTY: /^\-{2}/,
-  COLOR_CODE: /^rgb\(|^#|^hsl\(/,
-  OBJECT: /\(([\w,\s\(\)]*)\{(\S*[\s+:\S+\s+,*\S+]*)\}\)/,
   GLOBAL: /:global\((.*?)\)/,
   SPLIT: (syntax: string, flags?: string) => new RegExp(`\\s*\\${syntax}\\s*`, flags),
 };
