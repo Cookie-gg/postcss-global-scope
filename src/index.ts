64d45430a6fa20e315e6f3307d6cea389fd7c8c0
cookie_gg <cookie.gg.dev@gmail.com>
2023-01-30 12:31:54 +0900

    - add strict scope option - use yarn

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,13 @@ import postcss, { Rule } from 'postcss';
 import type * as PostCSS from 'postcss';
 import { REGEXP } from './libs/regexp';
 
-type Options = { skip?: string; classes?: string[]; cssModule?: boolean };
+type Options = { skip?: string; classes?: string[]; cssModule?: boolean; strictScope?: boolean };
 type Plugin = (decl: PostCSS.Declaration, opts: Required<Options>) => void;
 
 const SPLIT_REGEXP = REGEXP.SPLIT('^');
-const defaults: Required<Options> = { skip: '-', classes: [], cssModule: false };
+const defaults: Required<Options> = { skip: '-', classes: [], cssModule: false, strictScope: false };
 
-const plugin: Plugin = (decl, { skip, classes, cssModule }) => {
+const plugin: Plugin = (decl, { skip, classes, cssModule, strictScope }) => {
   const { value: values, parent, prop, important } = decl;
   const selector = (parent as Rule).selector;
   const selectorDetails = selector.match(REGEXP.GLOBAL);
@@ -17,7 +17,7 @@ const plugin: Plugin = (decl, { skip, classes, cssModule }) => {
       values.split(SPLIT_REGEXP).map((value, i) => {
         if (value !== skip) {
           const newRule = postcss.rule({
-            selector: `[class="${classes[i].replaceAll(/\./g, '')}"] ${selector}`,
+            selector: `[class${strictScope ? '' : '*'}="${classes[i].replaceAll(/\./g, '')}"] ${selector}`,
           });
           const newDecl = postcss.decl({ prop, value, important });
           newRule.append(newDecl);
